e786544d728ddc1efa9430640ccd01c7aa4b8d03
Andrei-David Nan <nandavid190@gmail.com>
2024-05-07 13:51:18 +0300

    v0.1.13 - cmake build improvments, add target run, cleanup, tested file output;

--- src/crawler.cpp
+++ src/crawler.cpp
@@ -1,8 +1,22 @@
 
 #include "crawler.h"
+#include <fstream>
 
 int main() {
     Crawler crawler;
     crawler.crawl();
+
+    std::ofstream outputFile("results.txt", std::ios::trunc); 
+     std::string testString = "Test string\n";
+
+    if (outputFile) {
+        std::string results = testString;;
+        outputFile << results;
+        outputFile.close();
+    } else {
+        std::cerr << "Error: Unable to open results.txt for writing!" << std::endl;
+        return 1;
+    }
+
     return 0;
 }
