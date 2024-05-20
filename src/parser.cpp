61dac7df7a9295518d81ae3695f15ce285804b82
Andrei-David Nan <nandavid190@gmail.com>
2024-05-20 14:48:24 +0300

    v0.1.23 - fix crawling depth traversal issue, other improvements;

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,10 +3,11 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 const std::vector<std::string> URL_PREFIXES = {"https://", "http://"};
-const std::vector<std::string> URL_STARTS = {"href=\"", "href = \"", "http://", "https://"};
-const std::string URL_END_CHARS = "\"#?, ";
+const std::vector<std::string> URL_STARTS = {"href=\"", "href='", "src=\"", "src='", "url(", "http://", "https://"};
+const std::string URL_END_CHARS = "\"'#? ),";
 
 std::string getHostnameFromUrl(const std::string& url) {
     for (const auto& prefix : URL_PREFIXES) {
@@ -28,6 +29,7 @@ std::string getHostPathFromUrl(const std::string& url) {
     }
     return "/";
 }
+
 bool hasSuffix(const std::string& str, const std::string& suffix) {
     return str.size() >= suffix.size() && str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
 }
@@ -42,13 +44,10 @@ bool verifyDomain(const std::string& url) {
     return false;
 }
 
-
 bool verifyUrl(const std::string& url) {
     std::string urlDomain = getHostnameFromUrl(url);
     return urlDomain.empty() || verifyDomain(urlDomain);
 }
-
-
 
 bool verifyType(const std::string& url) {
     const std::string FORBIDDEN_TYPES[] = {".css", ".js", ".pdf", ".png", ".jpeg", ".jpg", ".ico"};
@@ -60,17 +59,16 @@ bool verifyType(const std::string& url) {
     return true;
 }
 
-
-
 std::string reformatHttpResponse(const std::string& httpText) {
-    const std::string ALLOWED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01233456789.,/\":#?+-_= ";
+    const std::string ALLOWED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,/\":#?+-_= ";
     std::map<char, char> charMap;
-    
+
     for (char ch : ALLOWED_CHARS) {
         charMap[ch] = ch;
     }
-    
+
     charMap['\n'] = ' ';
+    charMap['\t'] = ' ';
 
     std::string result;
     for (char ch : httpText) {
@@ -81,35 +79,47 @@ std::string reformatHttpResponse(const std::string& httpText) {
     return result;
 }
 
-
-
-
 std::vector<std::pair<std::string, std::string>> extractUrls(const std::string& httpText) {
     std::string httpRaw = reformatHttpResponse(httpText);
     
     std::vector<std::pair<std::string, std::string>> extractedUrls;
 
-    for (const auto& startText : URL_STARTS) {
-        size_t startPos = 0;
+    size_t startPos = 0;
 
-        while ((startPos = httpRaw.find(startText, startPos)) != std::string::npos)  {
-            startPos += startText.size();
-            size_t endPos = httpRaw.find_first_of(URL_END_CHARS, startPos);
+    while (startPos < httpRaw.size()) {
+        size_t foundPos = std::string::npos;
+        std::string foundUrl;
 
+        for (const auto& startText : URL_STARTS) {
+            foundPos = httpRaw.find(startText, startPos);
+            if (foundPos != std::string::npos) {
+                foundPos += startText.size();
+                break;
+            }
+        }
+
+        if (foundPos != std::string::npos) {
+            size_t endPos = httpRaw.find_first_of(URL_END_CHARS, foundPos);
             if (endPos == std::string::npos) {
                 endPos = httpRaw.size();
             }
+            foundUrl = httpRaw.substr(foundPos, endPos - foundPos);
 
-            std::string foundUrl = httpRaw.substr(startPos, endPos - startPos);
+            // Handling relative URLs
+            if (foundUrl.compare(0, 1, "/") == 0) {
+                foundUrl = "http://" + getHostnameFromUrl(httpRaw) + foundUrl;
+            } else if (foundUrl.compare(0, 2, "./") == 0) {
+                foundUrl = "http://" + getHostnameFromUrl(httpRaw) + "/" + foundUrl.substr(2);
+            }
 
-            if (verifyUrl(foundUrl)) {
+            if (verifyUrl(foundUrl) && verifyType(foundUrl)) {
                 extractedUrls.push_back({getHostnameFromUrl(foundUrl), getHostPathFromUrl(foundUrl)});
             }
-            
-            httpRaw.erase(0, endPos);
+            startPos = endPos;
+        } else {
+            break;
         }
     }
 
     return extractedUrls;
 }
-
